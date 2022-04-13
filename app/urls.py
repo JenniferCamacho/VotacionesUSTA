e83271caf15dce5378f9d7bd98cc5ebac1e1d940
Jennifer Camacho <jennifercamacho@usantotomas.edu.co>
2022-04-12 19:43:48 -0500

    se crean los links para navegar en el menu de decano

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,9 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'), 
     path('inicio/', views.inicio, name='inicio'),
+    path('menuDecano/', views.menuDecano, name='menuDecano'),
+    path('agregarEstudiante/', views.agregarEstudiante, name='agregarEstudiante'),
+    path('crearVotacion/', views.crearVotacion, name='crearVotacion'),
+    path('listaDeEstudiantes/', views.listaDeEstudiantes, name='listaDeEstudiantes'),
+    path('listaDeVotaciones/', views.listaDeVotaciones, name='listaDeVotaciones'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,18 @@ def index(request):
 
 def inicio(request):
     return render (request, 'app/inicio.html')
+
+def menuDecano(request):
+    return render (request, 'app/menuDecano.html')
+
+def agregarEstudiante(request):
+    return render (request, 'app/agregarEstudiante.html')
+
+def crearVotacion(request):
+    return render (request, 'app/crearVotacion.html')
+
+def listaDeEstudiantes(request):
+    return render (request, 'app/listaDeEstudiantes.html')
+
+def listaDeVotaciones(request):
+    return render (request, 'app/listaDeVotaciones.html')
